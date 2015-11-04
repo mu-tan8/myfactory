c692e4fa4a8e58318c79e7a2da294c944d35fd9e
mu-tan8 <takka-y@hotmail.co.jp>
2015-11-04 22:20:07 +0900

    add effect.

--- dnd_Element/dragElement.js
+++ dnd_Element/dragElement.js
@@ -2,9 +2,12 @@
 /*	参考URL：http://www.tohoho-web.com/wwwxx002.htm	*/
 
 
-	var obj;
+	var obj , alpha , x , y;
 	catch_a_Element = function (ev){	//	マウスで要素を掴む(catch a ElementObject by mousedown)
 		obj = ev.target.parentElement;
+		alpha = obj.style.opacity;
+		x = obj.style.right;
+		y = obj.style.bottom;
 		if (obj != oDiv){
 			obj = null;	//		移動させる要素でなければ空にする
 			return(ev);	//		イベントハンドラを有効にするために必ず真を返すこと(required to enable Event Handler , return ture)
@@ -14,9 +17,13 @@
 		if (obj){
 			obj.style.right = '' + (innerWidth - ev.clientX) + 'px';	//	右側の絶対座標　＝　ウィンドウの幅　－　ウィンドウ上でのマウスのＸ座標
 			obj.style.bottom = '' + (innerHeight - ev.clientY) + 'px';	//	下側の絶対座標　＝　ウィンドウの高さ　－　ウィンドウ上でのマウスのＹ座標
+			obj.style.opacity = 0.25;	//	移動中の要素の透明化（transmission effect）
 		}
 	}
 	drop_a_Element = function (ev){	//	要素を放す（ドロップ）(drop a ElementObject by mouseup)
+		obj.style.opacity = alpha;
+		x = obj.style.right;
+		y = obj.style.bottom;
 		obj = null;
 		return(ev);
 	}
