5f1c9356d98be96993a50aeeb5af719d4efbe977
mu-tan8 <takka-y@hotmail.co.jp>
2016-05-25 18:28:28 +0900

    bug fixed

--- DHTML/syncCloud/httpscan.js
+++ DHTML/syncCloud/httpscan.js
@@ -1,191 +1,144 @@
-
+
 //from new XMLHttpRequest() Example by MDN	https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Promise 
 
-//http(URL : string) : method (Require)
-//method? ( get | head | post | delete | put )(args : argsObject): Promise { head : <'http-headers'> , body : < http-contents > }
-/*
-opt? argsObject = {
-	setHeaders : { <'setRequestHeaderName' : 'value' , ... > } ,
-	getHeaders : { <'getResponseHeaderName' : null , ... > } ,
-	responseType : 'xhr.responseType' ,
-	authUser : 'auth UserName String',
-	authPW : 'auth PassWord String' ,
-	sendData : <sendDatas>
-}
-*/
+/*
+opt? argsObject = {
+	setHeaders : { <'setRequestHeaderName' : 'value' , ... > } ,
+	getHeaders : { <'getResponseHeaderName' : null , ... > } ,
+	responseType : <'xhr.responseType'> ,
+	authUser : 'auth UserName String',
+	authPW : 'auth PassWord String' ,
+	sendData : <sendDatas>
+}
+*/
+//method? ( get | head | post | delete | put )(args : argsObject): Promise { head : <'http-headers'> , body : < http-contents > }
+//http(URL : string) : method (Require)
+
+function http(URL){
+	var core = {
+		'ajax' : function (method , URL , args){
+			return new Promise(function(resolve,reject){
+				var xhr = new XMLHttpRequest();
+				xhr.onload = function(){
+					if (this.status >= 200 && this.status < 300){
+						var headers = {};
+						if ('getHeaders' in args){
+							for (var name in args['getHeaders']){
+								headers[name] = xhr.getResponseHeader(name);
+								name = null;
+							}
+						}
+						resolve({
+							'head' : headers ,
+							'body' : xhr.responseXML || xhr.response
+						});
+					}else{
+						reject(xhr.statusText);
+					}
+					xhr = null;
+				}
+				xhr.onerror = function(){
+					reject(xhr.statusText);
+					xhr = null;
+				}
+				if ('responseType' in args){
+					xhr.responseType = args['responseType'];
+				}
+				if ('setHeaders' in args){
+					for (var name in args['setHeaders']){
+						if (name == 'Content-Type' && xhr.overrideMimeType){
+							xhr.overrideMimeType(args['setHeaders'][name]);
+						}else{
+							xhr.setRequestHeader(name , args['setHeaders'][name]);
+						}
+						name = null;
+					}
+				}
+				xhr.open(method , URL , true , ('authUser' in args) ? args['authUser'] : '' , ('authPW' in args) ? args['authPW'] : '');
+				xhr.send(('sendData' in args) ? args['sendData'] : null);
+			});
+		}
+	}
+	return {
+		'get' : function(args){
+			return core.ajax('get' , URL , args);
+		} , 
+		'head' : function(args){
+			return core.ajax('head' , URL , args);
+		} ,
+		'post' : function(args){
+			return core.ajax('post' , URL , args);
+		} ,
+		'delete' : function(args){
+			return core.ajax('delete' , URL , args);
+		} ,
+		'put' : function(args){
+			return core.ajax('put' , URL , args);
+		}
+	}
+}
 
-function http(URL){
-	var core = {
-		'ajax' : function (method , URL , args){
-			return new Promise(function(resolve,reject){
-				var xhr = new XMLHttpRequest();
-				xhr.onload = function(){
-					if (this.status >= 200 && this.status < 300){
-						var headers = {};
-						if ('getHeaders' in args){
-							for (var name in args['getHeaders']){
-								headers[name] = xhr.getResponseHeader(name);
-								name = null;
-							}
-						}
-						resolve({
-							'head' : headers ,
-							'body' : xhr.responseXML || xhr.response
-						});
-					}else{
-						reject(xhr.statusText);
-					}
-					xhr = null;
-				}
-				xhr.onerror = function(){
-					reject(xhr.statusText);
-					xhr = null;
-				}
-				if ('responseType' in args){
-					xhr.responseType = args['responseType'];
-				}
-				if ('setHeaders' in args){
-					for (var name in args['setHeaders']){
-						if (name == 'Content-Type' && xhr.overrideMimeType){
-							xhr.overrideMimeType(args['setHeaders'][name]);
-						}else{
-							xhr.setRequestHeader(name , args['setHeaders'][name]);
-						}
-						name = null;
-					}
-				}
-				xhr.open(method , URL , true , ('authUser' in args) ? args['authUser'] : '' , ('authPW' in args) ? args['authPW'] : '');
-				xhr.send(('sendData' in args) ? args['sendData'] : null);
-			});
-		}
-	}
-	return {
-		'get' : function(args){
-			return core.ajax('get' , URL , args);
-		} , 
-		'head' : function(args){
-			return core.ajax('head' , URL , args);
-		} ,
-		'post' : function(args){
-			return core.ajax('post' , URL , args);
-		} ,
-		'delete' : function(args){
-			return core.ajax('delete' , URL , args);
-		} ,
-		'put' : function(args){
-			return core.ajax('put' , URL , args);
-		}
-	}
-}
-
-//	doesn't work. want help 
+// File = {name: string , {@type: string @size: integer , @time: date} }
+// Dir = {[name : strings]: File | Dir}
+// dirRequest(URL: string) : Promise<Dir>
 function dirRequest(URL){
-	return http(URL)
-	.get({
-		'setHeaders' : {
-			'Content-Type' : 'text/html'
-		} , 
-		'responseType' : 'document'
-	})
-	.then(function(resource){
-		//return Promise.all(
-			resource.body.getElementsByTagName('a').filter(function(link){
-				return (link.getAttribute('href') == link.firstChild.nodeValue);
-			})
-			.map(function(link){
-					if (/\/$/.test(link.getAttribute('href'))){
-						return dirRequest(URL+link.getAttribute('href'))
-						.then(function(dir){
-							return {
-								'name' : link.getAttribute('href') ,
-								'attr' : dir
-							}
-						});
-					}else{
-						return fileRequest(URL+link.getAttribute('href'))
-						.then(function(resource){
-							return {
-								'name' : link.getAttribute('href') ,
-								'attr' : {
-									'@type': resource.head['Content-Type'] ,
-									'@size' : resource.head['Content-Length'] ,
-									'@time' : resource.head['Last-Modified']
-								}
-							}
-						});
+	//directory scaner
+	return http(URL)
+	.get({
+		'setHeaders' : {
+			'Content-Type' : 'text/html'
+		} , 
+		'responseType' : 'document'
+	})
+	.then(function(resource){
+		var link = resource.body.getElementsByTagName('a');
+		var array = [] , j = 0;
+		for (var i = 0;i < link.length;i++){
+			if (link[i].getAttribute('href') == link[i].firstChild.nodeValue){
+				array[j] = link[i].getAttribute('href');
+				j++;
+			}
+		}
+		return array;
+	}).then(function(links){
+		return Promise.all(
+		links.map(function(link){
+			if (/\/$/.test(link)){
+				return dirRequest(URL+link)
+				.then(function(dir){
+					return {name: link.slice(0 , -1) , type: dir};
+				});
+			}else{
+				return fileRequest(URL+link)
+				.then(function(file){
+					return {
+						name: link ,
+						type: {
+							'@type' : file.head['Content-Type'] ,
+							'@size' : parseInt(file.head['Content-Length']) ,
+							'@time' : new Date(file.head['Last-Modified'])
+						}
 					}
-			});
-		//);
-	})
-	.then(function(items){
-		return items.reduce(function(o,item){
-			o[item['name']] = item['attr'];
+				});
+			}
+		}));
+	}).then(function(items){
+		return items.reduce(function(o , item){
+			o[item.name] = item.type;
 			return o;
-		});
-	});
-}
-
-//	doesn't work. want help
+		} , {});
+	});
+}
+
+// fileRequest(URL: string) : Promise {'Content-Type': string , 'Content-Length': , 'Last-Modified': string}
 function fileRequest(URL){
-	return http(URL)
-	.head({
-		'getHeaders' : {
-			'Content-Type' : null ,
-			'Content-Length' : null ,
-			'Last-Modified' : null
-		}
-	});
-}
-
-/*
-function dirRequest(URL){
-	//Directory scanning
-	var xhr = new XMLHttpRequest();
-
-	xhr.onload = function (){
-		var links = xhr.responseXML.getElementsByTagName('a');
-		for (var i = 0;i < links.length;i++){
-			if (links[i].getAttribute('href') == links[i].firstChild.nodeValue){
-				if (/\/$/.test(links[i].getAttribute('href'))){
-					console.log(URL+links[i].firstChild.nodeValue);
-					dirRequest(URL+links[i].getAttribute('href'));
-				}else{
-					fileRequest(URL,links[i].getAttribute('href'));
-				}
-			}
-		}
-	}
-	xhr.onerror = function (e){
-		alert(xhr.status);
-	}
-	xhr.open('get',URL,true);
-	xhr.responseType = 'document';
-	if (xhr.overrideMimeType){
-		xhr.overrideMimeType('text/html');
-	}else{
-		xhr.setRequestHeader('Content-Type','text/html');
-	}
-	xhr.send(null);
-}
-
-
-function fileRequest(URL,entry){
-	//File Scanning
-	var xhr = new XMLHttpRequest();
-	xhr.onload = function (){
-			headers = {'fileType':'Content-Type','fileSize':'Content-Length','lastModified':'Last-Modified'};
-			for (var name in headers){
-				headers[name] = xhr.getResponseHeader(headers[name]);
-			}
-			headers['fileName'] = entry;
-			console.log('"'+URL+entry+'":'+JSON.stringify(headers));
-			xhr = null;
-	}
-	xhr.onerror = function (e){
-		alert(xhr.status);
-	}
-	xhr.open('head',URL+entry,true);
-	xhr.send(null);
-}
-*/
-
+	//file scaner
+	return http(URL)
+	.head({
+		'getHeaders' : {
+			'Content-Type' : null ,
+			'Content-Length' : null ,
+			'Last-Modified' : null
+		}
+	});
+}
--- DHTML/syncCloud/sync.js
+++ DHTML/syncCloud/sync.js
@@ -1,22 +1,35 @@
 function Sync(URL){
+	var LocalTime = new Date().toGMTString();
 	return http(URL)
 	.head({ 'getHeaders' : { 'Date' : null } } )
 	.then(function(res){
 
-		var LocalTime = new Date().toGMTString();
 		var ServerTime = res.head['Date'];
 
 		//Files on Server
-		var oSFS = dirRequest(URL)
-		.then(function(o){return o})
-		.error(console.error.bind(console));
+		var oSFS;
+		dirRequest(URL)
+		.then(function(o){return oSFS = o})
+		.catch(console.error.bind(console));
+		console.log('LT:'+LocalTime);
 
 		//Files on Local
-		var oCFS = tree('/')
-		.then(function(o){return o})
+		var oCFS;
+		tree('/')
+		.then(function(o){return oCFS = o})
 		.catch(console.error.bind(console));
 
-		return true;
+		console.log('ST:'+ServerTime);
+
+		setTimeout(function(){
+			if (!oSFS || !oCFS ){
+				setTimeout(arguments.callee,1);
+			}else{
+				console.log('"ServerFiles":'+JSON.stringify(oSFS , null , "\t"));	//file sync compare process
+			}
+		},1);
+		console.log('offset:'+(new Date(LocalTime).getTime() - new Date(ServerTime).getTime()));
+		return res;
 	})
 	.catch(console.error.bind(console));
 }
